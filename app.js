ea65a8bda66505660aafc1677bb5279afe45d7b0
Маркіян <jtcjfi@gmail.com>
2026-03-11 17:23:56 +0200

    homework done

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const http = require('http')
+
+const server = http.createServer((rep,res) =>{
+    res.end('Hello node')
+})
+server.listen(5500, ()=>{
+    console.log('Node server is working!')
+})
